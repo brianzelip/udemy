ed94ca181b8ee319aeb2d504665f99efa6ca4062
Brian Zelip <bzelip@gmail.com>
2018-05-25 16:24:41 -0400

    rename project 1 directory, start new Vue instance and add basic player data and UI logic

---
+++ project 1 - MonsterSlayer/app.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '[data-vue]',
+  data: {
+    msg: 'hello',
+    player: {
+      health: 100
+    },
+    monster: {
+      health: 100
+    },
+    newGame: false
+  },
+  methods: {},
+  computed: {}
+});
