1a7763401c429bd233b15d2ae3df3c14ab01a125
Brian Zelip <bzelip@gmail.com>
2018-05-26 12:24:10 -0400

    create attack method to take min and max values for attack and special attack buttons in index.html

--- vuejs2/project 1 - MonsterSlayer/app.js
+++ vuejs2/project 1 - MonsterSlayer/app.js
@@ -17,6 +17,13 @@ new Vue({
       this.monster.health = 100;
       this.newGame = !this.newGame;
       return;
+    },
+    attack: function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      const damage = Math.floor(Math.random() * (max - min + 1)) + min;
+      console.log(`${min > 0 ? 'SPECIAL ' : ''}DAMAGE IS ${damage}`);
+      return damage;
     }
   },
   computed: {}
