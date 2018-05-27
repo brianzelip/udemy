bc4c181f4f10a596ae5ae51b51320559864fb692
Brian Zelip <bzelip@gmail.com>
2018-05-27 10:44:23 -0400

    add comment link to source of random num generation in vm.attack()

--- vuejs2/project 1 - MonsterSlayer/app.js
+++ vuejs2/project 1 - MonsterSlayer/app.js
@@ -19,6 +19,7 @@ new Vue({
       return;
     },
     attack: function(min, max) {
+      // via https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
       min = Math.ceil(min);
       max = Math.floor(max);
       const damage = Math.floor(Math.random() * (max - min + 1)) + min;
