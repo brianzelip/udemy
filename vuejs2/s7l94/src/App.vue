98cd409504b17b16cee963f17c0955d9c55f369b
Brian Zelip <bzelip@gmail.com>
2018-06-02 12:49:17 -0400

    add basic example of root component in App.vue

--- vuejs2/s7l94/src/App.vue
+++ vuejs2/s7l94/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
-    <h1>hello world</h1>
+    <h1>Status: {{ status }}</h1>
+    <button @click="changeStatus">Change Status</button>
   </div>
 </template>
 
 <script>
+export default ('app',
+{
+  data: function() {
+    return {
+      status: 'All Good'
+    };
+  },
+  methods: {
+    changeStatus: function() {
+      this.status = 'All Bad';
+    }
+  }
+});
 </script>
