065eef5af6a85b8f591f5676f5a0ede3bf47a841
Brian Zelip <bzelip@gmail.com>
2018-06-02 13:01:41 -0400

    mv s7l94 s7l94-95

---
+++ vuejs2/s7l94-95/src/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Status: {{ status }}</h1>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'All Good'
+    };
+  },
+  methods: {
+    changeStatus: function() {
+      this.status = 'All Bad';
+    }
+  }
+};
+</script>
