6a8d537e35eb6906d9b272bbee1d6d7279caad16
Brian Zelip <bzelip@gmail.com>
2018-06-03 08:32:49 -0400

    make new status-changer component from template and methods in App-creating-a-component.vue

--- vuejs2/s7l94-95/src/App-creating-a-component.vue
+++ vuejs2/s7l94-95/src/App-creating-a-component.vue
@@ -1,23 +1,6 @@
 <template>
-  <div>
-    <h1>Status: {{ status }}</h1>
-    <button @click="changeStatus">Change Status</button>
-  </div>
+  <status-changer></status-changer>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      status: 'All Good'
-    };
-  },
-  methods: {
-    changeStatus: function() {
-      return this.status === 'All Good'
-        ? (this.status = 'All Bad')
-        : (this.status = 'All Good');
-    }
-  }
-};
 </script>
