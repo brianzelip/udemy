6935aa23fe5aac58ac185973c58308cf9b215768
Brian Zelip <bzelip@gmail.com>
2018-06-03 08:33:58 -0400

    import and register StatusChanger in main.js

--- vuejs2/s7l94-95/src/main.js
+++ vuejs2/s7l94-95/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App94 from './App-root-component.vue';
 import App95 from './App-creating-a-component.vue';
+import Status from './StatusChanger-creating-a-component.vue';
 import AppIndex from './App-index.vue';
+
+Vue.component('status-changer', Status);
 
 new Vue({
   el: '[data-vue="root-component"]',
