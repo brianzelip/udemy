09f9c08efa5304546913fe582f0179e6f1267e78
Brian Zelip <bzelip@gmail.com>
2018-06-03 11:07:34 -0400

    add local component App-local.vue

---
+++ vuejs2/s7l94-95/src/App-local.vue
@@ -0,0 +1,10 @@
+<template>
+  <status-changer></status-changer>
+</template>
+
+<script>
+import StatusChanger from './StatusChanger.vue';
+export default {
+  components: { 'status-changer': StatusChanger }
+};
+</script>
