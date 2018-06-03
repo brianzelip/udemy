2007a556caeceb3fb6a4ca99ecdad712c3115db1
Brian Zelip <bzelip@gmail.com>
2018-06-03 08:10:52 -0400

    create index.html and App-index.vue to make a landing page for this practice

---
+++ vuejs2/s7l94-95/src/App-index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <ol>
+      <li v-for="work in works" :key="work">
+        <a :href="work.url">{{ work.title }}</a>
+      </li>
+    </ol>
+  </main>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      title:
+        'Workspace for Section 7 Lecture 94 - Root Component, and Lecture 95 - Creating a Component',
+      works: {
+        '94': { title: 'Root component', url: 'root-component.html' },
+        '95': {
+          title: 'Creating a component',
+          url: 'creating-a-component.html'
+        }
+      }
+    };
+  }
+};
+</script>
