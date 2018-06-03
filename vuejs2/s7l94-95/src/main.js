dc74c12a66f99419914a76aa5cbf00a0407f0846
Brian Zelip <bzelip@gmail.com>
2018-06-03 08:11:23 -0400

    update main.js w/ new index app

--- vuejs2/s7l94-95/src/main.js
+++ vuejs2/s7l94-95/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App94 from './App-root-component.vue';
 import App95 from './App-creating-a-component.vue';
+import AppIndex from './App-index.vue';
 
 new Vue({
   el: '[data-vue="root-component"]',
@@ -9,4 +10,8 @@ new Vue({
 new Vue({
   el: '[data-vue="creating-a-component"]',
   render: h => h(App95)
+});
+new Vue({
+  el: '[data-vue="index"]',
+  render: h => h(AppIndex)
 });
