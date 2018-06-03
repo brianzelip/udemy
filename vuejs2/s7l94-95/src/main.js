9259749e2d09ed9251273d7c92673232f0afe6ca
Brian Zelip <bzelip@gmail.com>
2018-06-03 07:55:23 -0400

    update main.js w/ new vue imports and new Vues

--- vuejs2/s7l94-95/src/main.js
+++ vuejs2/s7l94-95/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App94 from './App-root-component.vue';
+import App95 from './App-creating-a-component.vue';
 
 new Vue({
-  el: '[data-vue]',
-  render: h => h(App)
+  el: '[data-vue="root-component"]',
+  render: h => h(App94)
+});
+new Vue({
+  el: '[data-vue="creating-a-component"]',
+  render: h => h(App95)
 });
