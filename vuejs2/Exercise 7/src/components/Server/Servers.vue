66c53d385d0ea5f9030b876bb1d58fa6728485de
Brian Zelip <bzelip@gmail.com>
2018-06-04 15:38:05 -0400

    update Servers template to pass props to <server>

--- vuejs2/Exercise 7/src/components/Server/Servers.vue
+++ vuejs2/Exercise 7/src/components/Server/Servers.vue
@@ -1,18 +1,33 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
-        </ul>
-    </div>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <server
+        v-for="server in servers"
+        :server="server"
+        :key="server.id"></server>
+    </ul>
+  </div>
 </template>
 
 <script>
+import server from './Server.vue';
+export default {
+  data() {
+    return {
+      servers: [
+        { id: 1, status: 'Normal' },
+        { id: 2, status: 'Critical' },
+        { id: 3, status: 'Unknown' },
+        { id: 4, status: 'Critical' },
+        { id: 5, status: 'Normal' }
+      ]
+    };
+  },
+  components: {
+    server
+  }
+};
 </script>
 
 <style>
-
 </style>
