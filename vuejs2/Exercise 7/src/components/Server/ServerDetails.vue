2d851781f9e259dce1969e501d86affc2291167b
Brian Zelip <bzelip@gmail.com>
2018-06-06 11:29:50 -0400

    add changeServerToNormal method in ServerDetails component, and a comment with references to how objects and arrays being passed around as props work in vue since they are reference types

--- vuejs2/Exercise 7/src/components/Server/ServerDetails.vue
+++ vuejs2/Exercise 7/src/components/Server/ServerDetails.vue
@@ -3,7 +3,7 @@
     <p v-if="!server">No server selected</p>
     <p v-else>Server #{{ server.id }} is currently {{ server.status }}</p>
     <hr>
-    <button @click="changeToNormal">Change to Normal</button>
+    <button @click="changeServerToNormal">Change to Normal</button>
   </div>
 </template>
 
@@ -22,7 +22,18 @@ export default {
     });
   },
   methods: {
-    changeToNormal() {}
+    changeServerToNormal() {
+      // this works without having to pass the new status around
+      // because objects and arrays in JavaScript are passed by reference,
+      // objects are reference type, therefore the objects in the main servers
+      // array (in Servers.vue) only exist in one place in memory for each object,
+      // so when a value there is changed in one place, that value is updated
+      // everywhere. See the tip in
+      // https://vuejs.org/v2/guide/components-props.html#One-Way-Data-Flow,
+      // or 9:12 min:sec mark in
+      // https://www.udemy.com/vuejs-2-the-complete-guide/learn/v4/t/practice/72/instructor-solution
+      this.server.status = 'Normal';
+    }
   }
 };
 </script>
