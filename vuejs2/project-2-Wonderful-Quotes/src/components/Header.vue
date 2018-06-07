20a973c970186112fc4d828890e4b73207a74fee
Brian Zelip <bzelip@gmail.com>
2018-06-07 15:43:45 -0400

    add Header component

---
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  header
+    h1 Quotes Added
+    .progress
+      .progress-bar(role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100")
+</template>
+
+<script>
+</script>
