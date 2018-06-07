b372053b0d9a834331ed20b750bc8f9bdf6a0fd4
Brian Zelip <bzelip@gmail.com>
2018-06-07 15:51:36 -0400

    add more components and an updated App.vue

--- vuejs2/project-2-Wonderful-Quotes/src/App.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/App.vue
@@ -1,13 +1,18 @@
-<template>
-    <div class="container">
-    
-    </div>
+<template lang="pug">
+  .container(style="margin-top: 3rem;")
+    app-header
+    app-footer
 </template>
 
 <script>
-    export default {
-        
-    }
+import appHeader from './components/Header.vue';
+import appFooter from './components/Footer.vue';
+export default {
+  components: {
+    appHeader,
+    appFooter
+  }
+};
 </script>
 
 <style>
