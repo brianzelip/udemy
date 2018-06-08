c1aea523846c2eb870eefa6c475bad1ea8e38fb5
Brian Zelip <bzelip@gmail.com>
2018-06-07 23:09:51 -0400

    adjust progress bar for bootstrap 4

--- vuejs2/project-2-Wonderful-Quotes/src/components/Header.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Header.vue
@@ -2,7 +2,7 @@
   header
     h1 Quotes Added
     .progress
-      .progress-bar(role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100")
+      .progress-bar(role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100") 25%
 </template>
 
 <script>
