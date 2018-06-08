e17e3253868a4d7c4e4aa0e47b6d7dbb9ab7b932
Brian Zelip <bzelip@gmail.com>
2018-06-08 09:38:49 -0400

    better Form component markup

--- vuejs2/project-2-Wonderful-Quotes/src/components/Form.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Form.vue
@@ -2,8 +2,9 @@
   .row.justify-content-center
     .col-6
       .form-group
-        label(for="") {{ msg }}
-        textarea.form-control(rows="3")
+        label(for="quote-input").font-weight-bold Quote
+        textarea.form-control.mb-3#quote-input(rows="3")
+        button.btn.btn-primary.d-block.mx-auto  Add Quote
 </template>
 
 <script>
