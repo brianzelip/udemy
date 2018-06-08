9081dc30c57da22246a7bc2e1a02092a52e2fe37
Brian Zelip <bzelip@gmail.com>
2018-06-07 23:10:14 -0400

    add beginning form markup to Form.vue

---
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Form.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .row.justify-content-center
+    .col-6
+      .form-group
+        label(for="") {{ msg }}
+        textarea.form-control(rows="3")
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello'
+    };
+  }
+};
+</script>
+
