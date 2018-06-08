4cdf9274ec9527d8f966d41d3678796fed05f0df
Brian Zelip <bzelip@gmail.com>
2018-06-07 23:10:31 -0400

    import and use Form component

--- vuejs2/project-2-Wonderful-Quotes/src/App.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   .container(style="margin-top: 3rem;")
     app-header
+    app-form
     app-footer
 </template>
 
 <script>
 import appHeader from './components/Header.vue';
 import appFooter from './components/Footer.vue';
+import appForm from './components/Form.vue';
 export default {
   components: {
     appHeader,
+    appForm,
     appFooter
   }
 };
