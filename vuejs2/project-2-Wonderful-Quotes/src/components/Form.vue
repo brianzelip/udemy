cbf7598188e166544b6f8e568ef91bdf683ea68b
Brian Zelip <bzelip@gmail.com>
2018-06-09 15:39:35 -0400

    create addQuote method that talks to quoteBus in Form.vue

--- vuejs2/project-2-Wonderful-Quotes/src/components/Form.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Form.vue
@@ -3,20 +3,23 @@
     form.col-6
       .form-group
         label(for="quote-input").font-weight-bold Quote
-        textarea.form-control.mb-3#quote-input(rows="3" v-model="input")
+        textarea.form-control.mb-3#quote-input(rows="3" v-model="newQuote")
         button.btn.btn-primary.d-block.mx-auto(@click.prevent="addQuote") Add Quote
 </template>
 
 <script>
+import { quoteBus } from '../main.js';
+
 export default {
   data() {
     return {
-      input: ''
+      newQuote: ''
     };
   },
   methods: {
     addQuote() {
-      alert('clicked!');
+      quoteBus.$emit('newQuoteAdded', this.newQuote);
+      this.newQuote = '';
     }
   }
 };
