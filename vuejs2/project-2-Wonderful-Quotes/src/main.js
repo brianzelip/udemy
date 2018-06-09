4b913793d223c412e2c25ca42b3fc38735c464e6
Brian Zelip <bzelip@gmail.com>
2018-06-09 15:38:46 -0400

    register quoteBus in main.js

--- vuejs2/project-2-Wonderful-Quotes/src/main.js
+++ vuejs2/project-2-Wonderful-Quotes/src/main.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+export const quoteBus = new Vue();
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
