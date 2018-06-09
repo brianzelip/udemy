9f3c5960b091935fbf16b0fd77a23a8e74d73108
Brian Zelip <bzelip@gmail.com>
2018-06-09 11:20:05 -0400

    create QuoteGrid component that gets a prop and passes data to child comp via slots

---
+++ vuejs2/project-2-Wonderful-Quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .row
+    appQuote(v-for="quote in quotes") {{ quote }}
+
+</template>
+
+<script>
+import appQuote from './Quote.vue';
+export default {
+  props: {
+    quotes: Array
+  },
+  components: { appQuote }
+};
+</script>
+
