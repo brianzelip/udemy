0f81142917e5f21b4cb9bfe9e99f302f59ed7f6a
Brian Zelip <bzelip@gmail.com>
2018-06-09 16:49:04 -0400

    deleteQuote method in Quote.vue emits to quoteBus when a quote is clicked

--- vuejs2/project-2-Wonderful-Quotes/src/components/Quote.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Quote.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .col-sm-6.col-md-4.col-lg-3.mb-4
+  .col-sm-6.col-md-4.col-lg-3.mb-4(@click="deleteQuote")
     .card.quote
       .card-body
         slot
@@ -7,6 +7,15 @@
 </template>
 
 <script>
+import { quoteBus } from '../main';
+
+export default {
+  methods: {
+    deleteQuote() {
+      quoteBus.$emit('quoteClickedForDeletion', this.$el.innerText);
+    }
+  }
+};
 </script>
 
 <style>
