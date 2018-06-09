6bbfb7967f16f8a9fedc9c792a3203627fd306df
Brian Zelip <bzelip@gmail.com>
2018-06-09 11:12:26 -0400

    add Quote.vue with slot for data passed in by parent component

---
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .col-sm-6.col-md-4.c0l-lg-3
+    .card.quote
+      .card-body
+        slot
+
+</template>
+
+<script>
+</script>
+
+<style>
+.quote {
+  font-family: 'Arizonia', cursive;
+  font-size: 2rem;
+  cursor: pointer;
+  background-color: azure;
+}
+.quote:hover {
+  background-color: lightskyblue;
+}
+</style>
+
