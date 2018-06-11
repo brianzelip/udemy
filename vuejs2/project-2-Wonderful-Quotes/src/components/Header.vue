a7fad6418da129affcd26eef8d4a45e854fd5361
Brian Zelip <bzelip@gmail.com>
2018-06-11 15:04:19 -0400

    register prop in Header, and update progress bar accordingly

--- vuejs2/project-2-Wonderful-Quotes/src/components/Header.vue
+++ vuejs2/project-2-Wonderful-Quotes/src/components/Header.vue
@@ -2,8 +2,16 @@
   header.mb-4
     h1 Quotes Added
     .progress
-      .progress-bar(role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100") 25%
+      .progress-bar(role="progressbar" :style="{width: `${quotesProgress.numQuotes * 10}%`}" :aria-valuenow="quotesProgress.numQuotes" aria-valuemin="0" aria-valuemax="10") {{ quotesProgress.innerHTML }}
 </template>
 
 <script>
+export default {
+  props: {
+    quotesProgress: {
+      type: Object,
+      required: true
+    }
+  }
+};
 </script>
