10edf55bdee301f054716b06418b8fcccad002eb
Brian Zelip <bzelip@gmail.com>
2018-06-12 22:14:53 -0400

    flesh out a bit of Form component

--- vuejs2/Exercise 9/src/components/Form.vue
+++ vuejs2/Exercise 9/src/components/Form.vue
@@ -1,5 +1,29 @@
 <template lang="pug">
-  div
+  // .card
+  //   .card-header
+  //     h4 Your Data
+  //   .card-body
+  //     p Full Name:
+  //     p Mail:
+  //     p Password:
+  //     p Store in Database?:
+  form
+    .form-group
+      label(for="firstName") First name
+      input(type="text" placeholder="Enter first name").form-control#firstName
+    .form-group
+      label(for="lastName") Last name
+      input(type="text" placeholder="Enter last name").form-control#lastName
+    .form-group
+      label(for="email") Email address
+      input(type="email" placeholder="Enter email").form-control#email
+    .form-group
+      label(for="exampleInputPassword1") Password
+      input(type="password" placeholder="Password").form-control#exampleInputPassword1
+    .form-check
+      input(type="checkbox" class="form-check-input" id="exampleCheck1")
+      label(class="form-check-label" for="exampleCheck1") Check me out
+    button(type="submit" class="btn btn-primary") Submit
     <!-- Exercise 1 -->
     <!-- Create a Signup Form where you retrieve the following Information -->
     <!-- Full Name (First Name + Last Name) -->
