81edecbe9c944715bb63a638ead88053d9c8ab9e
Brian Zelip <bzelip@gmail.com>
2018-06-17 09:07:03 -0400

    wire up Summary.vue to formBus, and render form data in real time

--- vuejs2/Exercise 9/src/components/Summary.vue
+++ vuejs2/Exercise 9/src/components/Summary.vue
@@ -3,8 +3,42 @@
     .card-header
       h4 Your Data
     .card-body
-      p Full Name:
-      p Mail:
-      p Password:
-      p Store in Database?:
+      p First Name: {{ firstName }}
+      p Last Name: {{ lastName }}
+      p Mail: {{ email }}
+      p Password: {{ password }}
+      p Store in Database?: {{ database }}
 </template>
+
+<script>
+import { formBus } from '../main';
+
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      database: ''
+    };
+  },
+  created() {
+    formBus.$on('firstNameWasChanged', firstName => {
+      this.firstName = firstName;
+    });
+    formBus.$on('lastNameWasChanged', lastName => {
+      this.lastName = lastName;
+    });
+    formBus.$on('emailWasChanged', email => {
+      this.email = email;
+    });
+    formBus.$on('passwordWasChanged', password => {
+      this.password = password;
+    });
+    formBus.$on('databaseWasChanged', database => {
+      this.database = database;
+    });
+  }
+};
+</script>
