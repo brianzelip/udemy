d80e591a036aca65bc28af6204569cac7935ef8b
Brian Zelip <bzelip@gmail.com>
2018-06-17 09:48:09 -0400

    wire up formBus and check for form submission status in App.vue

--- vuejs2/Exercise 9/src/App.vue
+++ vuejs2/Exercise 9/src/App.vue
@@ -1,22 +1,32 @@
 <template lang="pug">
   .container.mt-4
-    .row.justify-content-center
+    .row.justify-content-center(v-show="!formSubmitted")
       .col-12.col-sm-8
         appForm
-    hr
-    .row.justify-content-center
-      .col-12.col-sm-8.col-md-6
+    .row.justify-content-center(v-show="formSubmitted")
+      .col-12.col-sm-8
         appSummary
 </template>
 
 <script>
+import { formBus } from './main';
 import Form from './components/Form.vue';
 import Summary from './components/Summary.vue';
 
 export default {
+  data() {
+    return {
+      formSubmitted: false
+    };
+  },
   components: {
     appForm: Form,
     appSummary: Summary
+  },
+  created() {
+    formBus.$on('formWasSubmitted', () => {
+      this.formSubmitted = true;
+    });
   }
 };
 </script>
