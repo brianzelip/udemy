54b16e4b5b18bdd3b1dab5ff6c75ce122f11bf8f
Brian Zelip <bzelip@gmail.com>
2018-06-23 10:36:19 -0400

    refactor resize function, add scroll function

--- vuejs2/Exercise 10/src/App.vue
+++ vuejs2/Exercise 10/src/App.vue
@@ -5,7 +5,9 @@
         <h1>Directives Exercise</h1>
         <!-- Exercise -->
         <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-        <p v-window:resize>{{ width }}px wide x {{ height }}px high</p>
+        <p v-window:resize></p>
+        <p v-window:scroll></p>
+        <div style="height: 5000px"></div>
       </div>
     </div>
   </div>
@@ -16,11 +18,39 @@ export default {
   directives: {
     window: {
       bind(el, binding, vnode) {
-        binding.arg === 'resize' ? (window.onresize = rsFn) : null;
-        function rsFn() {
-          const w = window.innerWidth;
-          const h = window.innerHeight;
-          el.innerHTML = `${w}px width by ${h}px height`;
+        binding.arg === 'resize'
+          ? resize()
+          : binding.arg === 'scroll' ? scroll() : null;
+
+        function resize() {
+          el.innerHTML = rsMsg();
+
+          window.onresize = rsFn;
+
+          function rsFn() {
+            el.innerHTML = rsMsg();
+          }
+          function rsMsg() {
+            const w = window.innerWidth;
+            const h = window.innerHeight;
+            return `The current browser window dimension is ${w}px wide by ${h}px tall`;
+          }
+        }
+
+        function scroll() {
+          el.innerHTML = sMsg();
+          el.style.position = 'fixed';
+
+          window.onscroll = sFn;
+
+          function sMsg() {
+            return `The current scroll position along the y axis is ${
+              window.scrollY
+            }`;
+          }
+          function sFn() {
+            el.innerHTML = sMsg();
+          }
         }
       }
     }
