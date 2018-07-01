cc355343e0ada5589b1dfd378f0319dea9fb64d0
Brian Zelip <bzelip@gmail.com>
2018-07-01 08:41:12 -0600

    vue init project 3

---
+++ vuejs2/project-3-Super-Quiz/src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  main.container
+    h1.center The Super Quiz
+    hr
+    app-question
+
+</template>
+
+<script>
+import Question from './components/Question.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    appQuestion: Question
+  }
+};
+</script>
+
---
+++ vuejs2/project-3-Super-Quiz/src/components/Question.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  section.col-6.mx-auto.border.rounded
+    .p2.bg-gray
+      h1.m0 hello
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
