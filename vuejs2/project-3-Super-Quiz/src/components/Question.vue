d34513c36813a9c262911a7491ca7e5d6daeddb4
Brian Zelip <bzelip@gmail.com>
2018-07-02 15:10:31 -0500

    render qData props in Question.vue

--- vuejs2/project-3-Super-Quiz/src/components/Question.vue
+++ vuejs2/project-3-Super-Quiz/src/components/Question.vue
@@ -1,24 +1,19 @@
 <template lang="pug">
   section.col-6.mx-auto.border.rounded
     .p2.bg-silver.rounded-top
-      h1.h3.m0.center What is 12+3?
+      h1.h3.m0.center {{ qData.question }}
     .p2.rounded-bottom
       ul.list-reset.mt2.flex.flex-wrap
-        li.col-6.flex.flex-center.mb2
-          button.mx-auto.h2.btn.btn-primary 45
-        li.col-6.flex.flex-center.mb2
-          button.mx-auto.h2.btn.btn-primary 65
-        li.col-6.flex.flex-center
-          button.mx-auto.h2.btn.btn-primary 3
-        li.col-6.flex.flex-center
-          button.mx-auto.h2.btn.btn-primary 67
+        li.col-6.flex.flex-center.mb2(v-for="choice in qData.ranChoices")
+          button.mx-auto.h2.btn.btn-primary {{ choice }}
 </template>
 
 <script>
 export default {
   data() {
     return {};
-  }
+  },
+  props: ['qData']
 };
 </script>
 
