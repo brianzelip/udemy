72990ae651c9076c437c314bddf556b7b25881f8
Brian Zelip <bzelip@gmail.com>
2018-07-02 02:44:57 -0600

    register Answer component in App.vue

--- vuejs2/project-3-Super-Quiz/src/App.vue
+++ vuejs2/project-3-Super-Quiz/src/App.vue
@@ -3,18 +3,21 @@
     h1.center The Super Quiz
     hr
     app-question
+    app-answer
 
 </template>
 
 <script>
 import Question from './components/Question.vue';
+import Answer from './components/Answer.vue';
 
 export default {
   data() {
     return {};
   },
   components: {
-    appQuestion: Question
+    appQuestion: Question,
+    appAnswer: Answer
   }
 };
 </script>
