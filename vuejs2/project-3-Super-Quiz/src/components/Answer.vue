086c86cdee2d46632fb47acd065d5f5902255135
Brian Zelip <bzelip@gmail.com>
2018-07-02 02:44:12 -0600

    Answer layout

---
+++ vuejs2/project-3-Super-Quiz/src/components/Answer.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  section.col-6.mx-auto.border.rounded.bg-aqua
+    .box
+      h1.mt0.purple That's correct! 🎉
+      button.btn.btn-primary Play again
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+section {
+  height: 13rem;
+}
+.box {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+</style>
